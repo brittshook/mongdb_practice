3a180e43d08ee041ea44e4dc4d45d66dc8b0dc3a
Britt Shook <brittanysshook@gmail.com>
2024-02-02 13:34:12 -0500

    add routes, convert to mongoose

--- routes/grades.mjs
+++ routes/grades.mjs
@@ -1,16 +1,126 @@
 import express from "express";
-import db from "../db/conn.mjs";
-import { ObjectId } from "mongodb";
+import Grade from "../models/grade.mjs";
 
 const router = express.Router();
 
-router.get("/:id", async (req, res) => {
-  let collection = await db.collection("grades");
-  let query = { _id: ObjectId(req.params.id) };
-  let result = await collection.findOne(query);
+router.post("/", async (req, res) => {
+  let newDocument = req.body;
 
-  if (!result) res.send("Not found").status(404);
-  else res.send(result).status(200);
+  if (newDocument.student_id) {
+    newDocument.learner_id = newDocument.student_id;
+    delete newDocument.student_id;
+  }
+
+  const result = await Grade.insertOne(newDocument);
+  res.status(204).json(result);
+});
+
+router.get("/:id", async (req, res) => {
+  const objId = req.params.id;
+  const result = await Grade.findById(objId);
+
+  if (!result) res.json({ error: { error: "Not found" } }).status(404);
+  else res.status(200).json(result);
+});
+
+router.patch("/:id/add", async (req, res) => {
+  const objId = req.params.id;
+  let result = await Grade.updateOne(
+    { _id: objId },
+    {
+      $push: { scores: req.body },
+    }
+  );
+
+  if (!result || result.acknowledged == false)
+    res.json({ error: "Not found" }).status(404);
+  else res.status(200).json(result);
+});
+
+router.patch("/:id/remove", async (req, res) => {
+  const objId = req.params.id;
+  let result = await Grade.updateOne(
+    { _id: objId },
+    {
+      $pull: { scores: req.body },
+    }
+  );
+
+  if (!result || result.acknowledged == false)
+    res.json({ error: "Not found" }).status(404);
+  else res.status(200).json(result);
+});
+
+router.delete("/:id", async (req, res) => {
+  const objId = req.params.id;
+  let result = await Grade.deleteOne({ _id: objId });
+
+  if (!result || result.deletedCount == 0)
+    res.json({ error: "Not found" }).status(404);
+  else res.status(204).json();
+});
+
+router.get("/student/:id", async (req, res) => {
+  res.redirect(`../learner/${req.params.id}`);
+});
+
+router.get("/learner/:id", async (req, res) => {
+  const learnerId = req.params.id;
+  let query = { learner_id: Number(learnerId) };
+
+  if (req.query.class) query.class_id = Number(req.query.class);
+
+  let result = await Grade.find(query);
+
+  if (!result) res.json({ error: "Not found" }).status(404);
+  else res.status(200).json(result);
+});
+
+router.delete("/learner/:id", async (req, res) => {
+  const learnerId = req.params.id;
+
+  let result = await Grade.deleteOne({ learner_id: Number(learnerId) });
+
+  if (!result || result.deletedCount == 0)
+    res.json({ error: "Not found" }).status(404);
+  else res.status(204).json();
+});
+
+router.get("/class/:id", async (req, res) => {
+  const classId = req.params.id;
+  let query = { class_id: Number(classId) };
+
+  if (req.query.learner) query.learner_id = Number(req.query.learner);
+
+  let result = await Grade.find(query);
+
+  if (!result) res.json({ error: "Not found" }).status(404);
+  else res.status(200).json(result);
+});
+
+router.patch("/class/:id", async (req, res) => {
+  const classId = req.params.id;
+
+  let result = await Grade.updateMany(
+    { class_id: Number(classId) },
+    {
+      $set: { class_id: req.body.class_id },
+    }
+  );
+
+  if (!result || result.acknowledged == false)
+    res.json({ error: "Not found" }).status(404);
+  else res.status(200).json(result);
+});
+
+router.delete("/class/:id", async (req, res) => {
+  const classId = req.params.id;
+
+  let result = await Grade.deleteMany({ class_id: Number(classId) });
+
+  if (!result || result.deletedCount == 0)
+    res.json({ error: "Not found" }).status(404);
+  else res.status(204).json();
 });
 
 export default router;
